b43d64d25c36a1ceecfef93b6e1783062294fe9b
Alonso <alonso.sandoval@alumnos.uva.es>
2023-09-22 12:58:49 +0200

    cambiado ./

--- 42-CURSUS/Libft/ft_split.c
+++ 42-CURSUS/Libft/ft_split.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "stdlib.h"
+#include "./stdlib.h"
 static int	count_words(const char *str, char c)
 {
 	int			flag;
