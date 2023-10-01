d4539f0990ae8c139fbf8354c36b801d657e8d34
Alonso <alonso.sandoval@alumnos.uva.es>
2023-10-01 12:37:06 +0200

    test7

--- 42-CURSUS/get_next_line/get_next_line.h
+++ 42-CURSUS/get_next_line/get_next_line.h
@@ -25,4 +25,5 @@ size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 #endif
--- 42-CURSUS/get_next_line/get_next_line_utils.c
+++ 42-CURSUS/get_next_line/get_next_line_utils.c
@@ -75,3 +75,30 @@ char	*ft_strdup(const char *s1)
 	str[i] = '\0';
 	return (str);
 }
+
+#include "get_next_line.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+
+	substr = (char *)malloc((len + 1) * sizeof(char));
+	if (!substr)
+		return (NULL);
+
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
