e2a03e11ca099b32dbed64e955ddb1a32d1092e2
Alonso <alonso.sandoval@alumnos.uva.es>
2023-10-01 17:26:25 +0200

    AA

--- 42-CURSUS/get_next_line/get_next_line_utils.c
+++ 42-CURSUS/get_next_line/get_next_line_utils.c
@@ -104,7 +104,11 @@ char	*ft_strjoin(char *s1, char *s2)
 	}
 	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
-		return (custom_free(&s1));
+	{
+		free(s1);
+		s1 = NULL;
+		return (NULL);
+	}
 	i = -1;
 	while (s1[++i])
 		str[i] = s1[i];
