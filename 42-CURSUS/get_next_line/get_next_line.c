476e3585d47e9eea1a17262809f62a76d91c6af1
Alonso <alonso.sandoval@alumnos.uva.es>
2023-10-01 12:54:26 +0200

    peurba borrada

--- 42-CURSUS/get_next_line/get_next_line.c
+++ 42-CURSUS/get_next_line/get_next_line.c
@@ -3,90 +3,108 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: asandova <asandova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/01 11:59:25 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/01 11:59:25 by marvin           ###   ########.fr       */
+/*   Created: 2023/09/27 11:43:54 by marvin            #+#    #+#             */
+/*   Updated: 2023/09/29 12:36:28 by asandova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	update_rest(char **line, char **rest)
+char	*custom_free(char **str)
 {
-	char	*newline;
-	char	*temp;
-
-	if (!*rest)
-		return;
-
-	newline = ft_strchr(*rest, '\n');
-	if (newline)
-	{
-		*rest = ft_strdup(newline + 1);
-		if (!*rest)
-			return; // Manejar error de asignación de memoria
-		temp = ft_substr(*line, 0, ft_strlen(*line) - ft_strlen(newline + 1));
-		free(*line);
-		if (!temp)
-		{
-			free(*rest);
-			*rest = NULL;
-			return; // Manejar error de asignación de memoria
-		}
-		*line = temp;
-	}
-	else
-	{
-		free(*rest);
-		*rest = NULL;
-	}
+	free(*str);
+	*str = NULL;
+	return (NULL);
 }
 
+char	*free_stored_line(t_fd_storage *fd_storage)
+{
+	char	*new_storage;
+	char	*ptr;
+	int		len;
 
-char	*read_line(int fd, char **rest, char *buffer, int *bytes_read)
+	ptr = ft_strchr(fd_storage->storage, '\n');
+	if (!ptr)
+		return (custom_free(&fd_storage->storage));
+	else
+		len = ptr - fd_storage->storage + 1;
+	if (!fd_storage->storage[len])
+		return (custom_free(&fd_storage->storage));
+	new_storage = ft_strdup(fd_storage->storage + len + 1);
+	free(fd_storage->storage);
+	if (!new_storage)
+		return (NULL);
+	fd_storage->storage = new_storage;
+	fd_storage->length -= len;
+	return (new_storage);
+}
+
+char	*ft_get_line(t_fd_storage *fd_storage)
 {
 	char	*line;
-	char	*temp;
+	char	*ptr;
+	int		len;
 
-	line = ft_strdup(*rest);
-	while (!ft_strchr(*rest, '\n') && (*bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0)
-	{
-		buffer[*bytes_read] = '\0';
-		temp = ft_strjoin(line, buffer);
-		free(line);
-		if (!temp)
-			return (NULL);
-		line = temp;
-	}
-	if (*bytes_read == -1)
-	{
-		free(line);
-		return (NULL); // Manejar error de lectura
-	}
-	update_rest(&line, rest);
+	ptr = ft_strchr(fd_storage->storage, '\n');
+	if (!ptr)
+		len = (int)fd_storage->length;
+	else
+		len = ptr - fd_storage->storage + 1;
+	line = ft_substr(fd_storage->storage, 0, len);
+	if (!line)
+		return (NULL);
 	return (line);
 }
 
+char	*read_file(int fd, t_fd_storage *fd_storage)
+{
+	int		bytes_read;
+	char	buffer[BUFFER_SIZE + 1];
+	char	*new_storage;
+
+	bytes_read = 1;
+	buffer[0] = '\0';
+	fd_storage->storage = ft_strdup("");
+	while (bytes_read > 0 && !ft_strchr(fd_storage->storage, '\n'))
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read > 0)
+		{
+			buffer[bytes_read] = '\0';
+			new_storage = ft_strjoin(fd_storage->storage, buffer);
+			free(fd_storage->storage);
+			if (!new_storage)
+				return (NULL);
+			fd_storage->storage = new_storage;
+			fd_storage->length += bytes_read;
+		}
+	}
+	if (bytes_read == -1 || fd_storage->length == 0)
+	{
+		free(fd_storage->storage);
+		return (NULL);
+	}
+	return (fd_storage->storage);
+}
 
 char	*get_next_line(int fd)
 {
-	static char	*rest;
-	char		*buffer;
-	char		*line;
-	int			bytes_read;
+	static t_fd_storage	fd_storage;
+	char				*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
+	fd_storage.storage = NULL;
+	fd_storage.length = 0;
+	if (fd < 0)
 		return (NULL);
-
-	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buffer)
+	if (!fd_storage.storage || !ft_strchr(fd_storage.storage, '\n'))
+		fd_storage.storage = read_file(fd, &fd_storage);
+	if (!fd_storage.storage)
 		return (NULL);
-
-	line = read_line(fd, &rest, buffer, &bytes_read);
-
-	free(buffer);
-	if (bytes_read == -1)
-		return (NULL);
+	line = ft_get_line(&fd_storage);
+	if (!line)
+		return (custom_free(&fd_storage.storage));
+	fd_storage.storage = free_stored_line(&fd_storage);
 	return (line);
 }
--- 42-CURSUS/get_next_line/get_next_line_utils.c
+++ 42-CURSUS/get_next_line/get_next_line_utils.c
@@ -3,46 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: asandova <asandova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/01 12:03:03 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/01 12:03:03 by marvin           ###   ########.fr       */
+/*   Created: 2023/09/27 12:42:11 by marvin            #+#    #+#             */
+/*   Updated: 2023/09/29 12:36:24 by asandova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	int		i;
-	int		j;
-	char	*str;
-
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-		str[i++] = s2[j++];
-	str[i] = '\0';
-	return (str);
-}
 
 char	*ft_strchr(const char *s, int c)
 {
@@ -59,21 +27,6 @@ char	*ft_strchr(const char *s, int c)
 	return ((char *)&s[i]);
 }
 
-char	*ft_strdup(const char *s1)
-{
-	char	*str;
-	int		i;
-
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
-	if (!str)
-		return (NULL);
-	i = -1;
-	while (s1[++i])
-		str[i] = s1[i];
-	str[i] = '\0';
-	return (str);
-}
-
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
@@ -82,8 +35,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	s_len = ft_strlen(s);
 	i = 0;
-	if (!s)
-		return (0);
 	if (start >= s_len)
 		return (ft_strdup(""));
 	if (len > s_len - start)
@@ -99,4 +50,60 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 	str[i] = '\0';
 	return (str);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
