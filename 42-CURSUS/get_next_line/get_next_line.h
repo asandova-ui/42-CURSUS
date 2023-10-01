563e10c9547ee6d672258a513a047152134f4038
Alonso <alonso.sandoval@alumnos.uva.es>
2023-10-01 14:02:34 +0200

    a ver si cuela ahora

--- 42-CURSUS/get_next_line/get_next_line.h
+++ 42-CURSUS/get_next_line/get_next_line.h
@@ -34,10 +34,10 @@ char	*ft_get_line(t_fd_storage *fd_storage);
 char	*read_file(int fd, t_fd_storage *fd_storage);
 char	*get_next_line(int fd);
 
-char	*ft_strdup(char *str);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_substr(char *str, unsigned int start, size_t len);
-char	*ft_strchr(char *str, int c);
+char	*ft_strdup(t_fd_storage *s1);
+char	*ft_strjoin(t_fd_storage *s1, char *s2);
+char	*ft_substr(t_fd_storage *s1, unsigned int start, size_t len);
+char	*ft_strchr(t_fd_storage *s1, int c);
 size_t	ft_strlen(char *str);
 
 #endif
--- 42-CURSUS/get_next_line/get_next_line_utils.c
+++ 42-CURSUS/get_next_line/get_next_line_utils.c
@@ -12,30 +12,30 @@
 
 #include "get_next_line.h"
 
-char	*ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(t_fd_storage *s1, char *s2)
 {
 	char	*str;
 	int		i;
 	int		c;
 
-	if (!s1)
+	if (!s1->storage)
 	{
-		s1 = (char *)malloc(sizeof(char));
-		if (!s1)
+		s1->storage = (char *)malloc(sizeof(char));
+		if (!s1->storage)
 			return (NULL);
-		s1[0] = '\0';
+		s1->storage[0] = '\0';
 	}
-	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	str = malloc(sizeof(char) * (s1->length + ft_strlen(s2) + 1));
 	if (!str)
-		return (custom_free(&s1));
+		return (custom_free(s1));
 	i = -1;
-	while (s1[++i])
-		str[i] = s1[i];
+	while (s1->storage[++i])
+		str[i] = s1->storage[i];
 	c = -1;
 	while (s2[++c])
 		str[i + c] = s2[c];
 	str[i + c] = '\0';
-	free(s1);
+	custom_free(s1);
 	return (str);
 }
 
@@ -49,15 +49,15 @@ size_t	ft_strlen(char *str)
 	return (i);
 }
 
-char	*ft_substr(char *str, unsigned int start, size_t len)
+char	*ft_substr(t_fd_storage *s1, unsigned int start, size_t len)
 {
 	size_t	i;
 	char	*res;
 
 	i = 0;
-	if (!str)
+	if (!s1->storage)
 		return (NULL);
-	if (start > ft_strlen(str))
+	if (start > s1->length)
 	{
 		res = (char *)malloc(sizeof(char));
 		if (!res)
@@ -65,43 +65,43 @@ char	*ft_substr(char *str, unsigned int start, size_t len)
 		*res = '\0';
 		return (res);
 	}
-	if (ft_strlen(str) - start < len)
-		len = ft_strlen(str) - start;
+	if (s1->length - start < len)
+		len = s1->length - start;
 	res = (char *)malloc(sizeof(char) * (len + 1));
 	if (!res)
 		return (NULL);
-	while (start < ft_strlen(str) && i < len && str[start])
-		res[i++] = str[start++];
+	while (start < s1->length && i < len && s1->storage[start])
+		res[i++] = s1->storage[start++];
 	res[i] = '\0';
 	return (res);
 }
 
-char	*ft_strchr(char *str, int c)
+char	*ft_strchr(t_fd_storage *s1, int c)
 {
 	int	i;
 
-	if (!str)
+	if (!s1->storage)
 		return (NULL);
 	i = -1;
-	while (str[++i])
-		if (str[i] == (char)c)
-			return (&((char *)str)[i]);
-	if (str[i] == (char)c)
-		return (&((char *)str)[i]);
+	while (s1->storage[++i])
+		if (s1->storage[i] == (char)c)
+			return (&((char *)s1->storage)[i]);
+	if (s1->storage[i] == (char)c)
+		return (&((char *)s1->storage)[i]);
 	return (0);
 }
 
-char	*ft_strdup(char *str)
+char	*ft_strdup(t_fd_storage *s1)
 {
 	char	*ret;
 	int		i;
 
-	ret = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1));
+	ret = (char *)malloc(sizeof(char) * (s1->length + 1));
 	if (!ret)
 		return (NULL);
 	i = -1;
-	while (str[++i])
-		ret[i] = str[i];
+	while (s1->storage[++i])
+		ret[i] = s1->storage[i];
 	ret[i] = '\0';
 	return (ret);
 }
