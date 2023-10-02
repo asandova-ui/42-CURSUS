9e614505a45a9da2934bbe49b15c6e16ee3e7836
Alonso <alonso.sandoval@alumnos.uva.es>
2023-10-02 17:02:39 +0200

    vsad

--- 42-CURSUS/printf/src/format_handlers/print_ptrhexa.c
+++ 42-CURSUS/printf/src/format_handlers/print_ptrhexa.c
@@ -3,26 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   print_ptrhexa.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asandova <asandova@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 17:27:27 by marvin            #+#    #+#             */
-/*   Updated: 2023/09/29 18:31:32 by asandova         ###   ########.fr       */
+/*   Updated: 2023/10/02 17:02:33 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/ft_printf.h"
 #include "../../libft/libft.h"
 
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+static void	ft_putnbr_hex(uintptr_t n, int fd)
+{
+	const char	*hex_chars = "0123456789abcdef";
+
+	if (n >= 16)
+		ft_putnbr_hex(n / 16, fd);
+	ft_putchar_fd(hex_chars[n % 16], fd);
+}
+
 int	print_ptrhexa(va_list args, int fd)
 {
-	void			*value;
-	char			*hex_value;
-	int				length;
+	uintptr_t	ptr_value = va_arg(args, uintptr_t);
+	int			chars_written = 0;
 
-	value = va_arg(args, void *);
-	hex_value = ft_itoa_base((unsigned long)value, "0123456789abcdef");
-	ft_putstr_fd(hex_value, fd);
-	length = ft_strlen(hex_value);
-	free((void *)hex_value);
-	return (length);
+	ft_putchar_fd('0', fd);
+	ft_putchar_fd('x', fd);
+	chars_written += 2;
+	ft_putnbr_hex(ptr_value, fd);
+	while (ptr_value != 0)
+	{
+		ptr_value /= 16;
+		chars_written++;
+	}
+	return (chars_written);
 }
