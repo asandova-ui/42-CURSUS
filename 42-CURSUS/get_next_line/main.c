20358b6183fcde13e808f1cea752b7cfab400563
asandova-ui <asandova@student.42madrid.com>
2023-10-03 16:19:24 +0200

    asd

---
+++ 42-CURSUS/get_next_line/main.c
@@ -0,0 +1,28 @@
+#include "get_next_line.h"
+
+int main(void)
+{
+	int i = 0;
+    int fd = open("archivo_prueba.txt", O_RDONLY);
+
+    if (fd == -1)
+    {
+        perror("Error al abrir el archivo");
+        return 1;
+    }
+	 while (i < 10)
+    {
+        char *line = get_next_line(fd);
+        if (line == NULL)
+        {
+			perror("Error al leer línea del archivo");
+            close(fd);
+            return 1;
+        }
+        printf("%s", line);
+        free(line);
+        i++;
+    }
+    close(fd);
+    return 0;
+}
