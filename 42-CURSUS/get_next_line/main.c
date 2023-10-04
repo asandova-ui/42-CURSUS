935ea7142e910d5d4cb5b9a37c5dd89cd313d1b3
asandova-ui <asandova@student.42madrid.com>
2023-10-04 09:24:49 +0200

    we

--- 42-CURSUS/get_next_line/main.c
+++ 42-CURSUS/get_next_line/main.c
@@ -2,27 +2,32 @@
 
 int main(void)
 {
+            printf("%s", "hooollalaaa");
+
 	int i = 0;
     int fd = open("archivo_prueba.txt", O_RDONLY);
 
     if (fd == -1)
     {
         perror("Error al abrir el archivo");
-        return 1;
+        return (0);
     }
+            printf("%s", "hooollalaaa");
+
 	 while (i < 10)
     {
+        printf("%s", "hooollalaaa");
         char *line = get_next_line(fd);
         if (line == NULL)
         {
 			perror("Error al leer línea del archivo");
             close(fd);
-            return 1;
+            return (0);
         }
         printf("%s", line);
         free(line);
         i++;
     }
     close(fd);
-    return 0;
+    return (0);
 }
