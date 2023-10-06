20eb64936f22f038904a3a96aeb75770e07b6fe8
asandova <asandova@student.42madrid.com>
2023-10-06 15:33:25 +0200

    comiteamos

--- 42-CURSUS/push_swap/src/algorithm.c
+++ 42-CURSUS/push_swap/src/algorithm.c
@@ -6,14 +6,14 @@
 /*   By: asandova <asandova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 12:47:03 by asandova          #+#    #+#             */
-/*   Updated: 2023/10/06 14:26:06 by asandova         ###   ########.fr       */
+/*   Updated: 2023/10/06 15:33:24 by asandova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 #include "../printf/includes/ft_printf.h"
 
-int	partition(t_stack *a, int low, int high)
+/*int	partition(t_stack *a, int low, int high)
 {
 	int	pivot;
 	int	i;
@@ -74,6 +74,43 @@ void	sort_stack(t_stack *a, t_stack *b)
 		i --;
 	}
 	free(temp_array);
+}*/
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low];
+    int i = low + 1;
+    int j = high;
+
+    while (1) {
+        while (i <= j && arr[i] <= pivot)
+            i++;
+
+        while (arr[j] >= pivot && j >= i)
+            j--;
+
+        if (j < i)
+            break;
+
+        swap(&arr[i], &arr[j]);
+    }
+
+    swap(&arr[low], &arr[j]);
+    return j;
+}
+
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(arr, low, high);
+
+        quicksort(arr, low, pivotIndex - 1);
+        quicksort(arr, pivotIndex + 1, high);
+    }
 }
 
 void	algorithm(t_stack *a, t_stack *b)
