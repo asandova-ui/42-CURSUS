36dc56a5ed5969122382e12d4478803ac8da7930
Alonso <alonso.sandoval@alumnos.uva.es>
2024-03-20 18:39:26 +0100

    z

--- 42-CURSUS/push_swap/includes/push_swap.h
+++ 42-CURSUS/push_swap/includes/push_swap.h
@@ -30,39 +30,39 @@ typedef struct s_list2
 //fichero main
 void	error_message(char *msg);
 int	main(int argc, char **argv);
-static void	sort_stack(t_list2 **stack_a, t_list2 **stack_b);
-static void	init_stack(t_list2 **stack, int argc, char **argv);
+//static void	sort_stack(t_list2 **stack_a, t_list2 **stack_b);
+//static void	init_stack(t_list2 **stack, int argc, char **argv);
 
 //fichero de operaciones
-static int	push(t_list2 **stack_to, t_list2 **stack_from);
+//static int	push(t_list2 **stack_to, t_list2 **stack_from);
 int	pa(t_list2 **stack_a, t_list2 **stack_b);
 int	pb(t_list2 **stack_a, t_list2 **stack_b);
 
-static int	reverse(t_list2 **stack);
+//static int	reverse(t_list2 **stack);
 int	rra(t_list2 **stack_a);
 int	rrb(t_list2 **stack_b);
 int	rrr(t_list2 **stack_a, t_list2 **stack_b);
 
-static int	rotate(t_list2 **stack);
+//static int	rotate(t_list2 **stack);
 int	ra(t_list2 **stack_a);
 int	rb(t_list2 **stack_b);
 int	rr(t_list2 **stack_a, t_list2 **stack_b);
 
-static int	swap(t_list2 **stack);
+//static int	swap(t_list2 **stack);
 int	sa(t_list2 **stack_a);
 int	sb(t_list2 **stack_b);
 int	ss(t_list2 **stack_a, t_list2 **stack_b);
 
 //control de errores
 void	error_control(int argc, char **argv);
-static char	**argc2(char *av);
-static int	ft_isnum(char *num);
-static int	ft_contains(int num, char **argv, int i);
+//static char	**argc2(char *av);
+//static int	ft_isnum(char *num);
+//static int	ft_contains(int num, char **argv, int i);
 
 //utils
 int	index_distance_head(t_list2 **stack, int index);
 int	get_min(t_list2 **stack, int val);
-static t_list2	*get_next_min(t_list2 **stack);
+//static t_list2	*get_next_min(t_list2 **stack);
 void	index_stack(t_list2 **stack);
 int	is_sorted(t_list2 **stack);
 
@@ -72,7 +72,7 @@ void	free_stack(t_list2 **stack);
 
 //algorithm and simple cases
 void	radix_sort(t_list2 **stack_a, t_list2 **stack_b);
-static int	get_max_bits(t_list2 **stack);
+//static int	get_max_bits(t_list2 **stack);
 void	simple_sort(t_list2 **stack_a, t_list2 **stack_b);
 void	sort_5(t_list2 **stack_a, t_list2 **stack_b);
 void	sort_4(t_list2 **stack_a, t_list2 **stack_b);
