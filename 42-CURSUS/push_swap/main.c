ffa0fb088dffeb3df8853de1bf40507937f4a371
Alonso <alonso.sandoval@alumnos.uva.es>
2024-03-20 16:50:47 +0100

    a

---
+++ 42-CURSUS/push_swap/main.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asandova <asandova@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 12:23:47 by asandova          #+#    #+#             */
+/*   Updated: 2023/11/03 12:42:42 by asandova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/push_swap.h"
+#include "printf/includes/ft_printf.h"
+
+void	error_message(char *msg)
+{
+	(void)msg;
+	write(2, "Error\n", 6);
+	exit(EXIT_FAILURE);
+}
+
+static void	init_stack(t_list **stack, int argc, char **argv)
+{
+	t_list	*new;
+	char	**args;
+	int		i;
+
+	i = 0;
+	if (argc == 2)
+	{
+		args = ft_split(argv[1], ' ');
+	}
+	else
+	{
+		i = 1;
+		args = argv;
+	}
+	while (args[i])
+	{
+		new = ft_lstnew(ft_atoi(args[i]));
+		ft_lstadd_back(stack, new);
+		i++;
+	}
+	index_stack(stack);
+	if (argc == 2)
+	{
+		free_string(args);
+	}
+}
+static void	sort_stack(t_list **stack_a, t_list **stack_b)
+{
+	if (ft_lstsize(*stack_a) <= 5)
+		simple_sort(stack_a, stack_b);
+	else
+		radix_sort(stack_a, stack_b);
+}
+
+int	main(int argc, char **argv)
+{
+	t_list	**stack_a;
+	t_list	**stack_b;
+
+	if (argc < 2)
+		return (-1);
+	error_control(argc, argv);
+	stack_a = (t_list **)malloc(sizeof(t_list));
+	*stack_a = NULL;
+	init_stack(stack_a, argc, argv);
+	if (is_sorted(stack_a) == 1)
+		exit (EXIT_SUCCESS);
+	stack_b = (t_list **)malloc(sizeof(t_list));
+	*stack_b = NULL;
+	sort_stack(stack_a, stack_b);
+	free_stack(stack_a);
+	free_stack(stack_b);
+	return (0);
+}
