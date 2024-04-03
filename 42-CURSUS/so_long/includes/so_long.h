86e51578df0e85bc20693b9de5604f8c56754b2c
Alonso <alonso.sandoval@alumnos.uva.es>
2024-04-03 17:46:28 +0200

    xc

--- 42-CURSUS/so_long/includes/so_long.h
+++ 42-CURSUS/so_long/includes/so_long.h
@@ -32,16 +32,16 @@
 # define IMG_HEIGHT			32
 # define IMG_WIDTH			32
 
-# define KEY_W				119
-# define KEY_A				97
-# define KEY_S				115
-# define KEY_D				100
+# define KEY_W				13
+# define KEY_A				0
+# define KEY_S				1
+# define KEY_D				2
 
-# define KEY_UP  			65362
-# define KEY_LEFT  			65361
-# define KEY_RIGHT 			65363
-# define KEY_DOWN  			65364
-# define KEY_ESC  			65307
+# define KEY_UP  			126
+# define KEY_LEFT  			125
+# define KEY_RIGHT 			123
+# define KEY_DOWN  			124
+# define KEY_ESC  			53
 
 # define WALL				'1'
 # define FLOOR 				'0'
