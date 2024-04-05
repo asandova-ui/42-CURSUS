3e4cf4ba896dda8e474b4c7ba9bd76ea390d719d
Alonso <alonso.sandoval@alumnos.uva.es>
2024-04-05 09:25:17 +0200

    sd

--- 42-CURSUS/so_long/src/map_proc/print_map.c
+++ 42-CURSUS/so_long/src/map_proc/print_map.c
@@ -68,7 +68,7 @@ void	ft_print_movements(t_game *game)
 
 	movements = ft_itoa(game->movements);
 	phrase = ft_strjoin("Movements : ", movements);
-	mlx_string_put(game->mlx_ptr, game->win_ptr, 40, 20, 0xFF0000, phrase);
+	mlx_string_put(game->mlx_ptr, game->win_ptr, 30, 20, 0xFF0000, phrase);
 	free(movements);
 	free(phrase);
 }
